3c0bd0cb878537126d515f72c000b88d81e9fe48
Kenneth Raahauge <kenneth.raahauge@gmail.com>
2025-11-26 14:52:37 +0100

    Added RNN

--- VAE_128.py
+++ VAE_128.py
@@ -1,3 +1,16 @@
+import math 
+import torch
+from torch import nn, Tensor
+from torch.nn.functional import softplus
+from torch.distributions import Distribution, Bernoulli
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision.transforms import ToTensor
+import torch.nn.functional as F
+import numpy as np
+from collections import defaultdict
+from typing import *
+
 class ConvolutionalVariationalAutoencoder(nn.Module):
     def __init__(self, input_shape:torch.Size, latent_features: int, in_channels: int = 1) -> None:
         super().__init__()
@@ -121,5 +134,29 @@ class ConvolutionalVariationalAutoencoder(nn.Module):
         return {"px": px, "pz": pz, "z": z}
 
 
-latent_features = 2
-# Example usage once you have tensors shaped as (batch, 1, H, W):
+class ReparameterizedDiagonalGaussian(Distribution):
+    """
+    A distribution `N(y | mu, sigma I)` compatible with the reparameterization trick given `epsilon ~ N(0, 1)`.
+    """
+    def __init__(self, mu: Tensor, log_sigma:Tensor):
+        assert mu.shape == log_sigma.shape, f"Tensors `mu` : {mu.shape} and ` log_sigma` : {log_sigma.shape} must be of the same shape"
+        self.mu = mu
+        self.sigma = log_sigma.exp()
+        
+    def sample_epsilon(self) -> Tensor:
+        """`\eps ~ N(0, I)`"""
+        return torch.empty_like(self.mu).normal_()
+        
+    def sample(self) -> Tensor:
+        """sample `z ~ N(z | mu, sigma)` (without gradients)"""
+        with torch.no_grad():
+            return self.rsample()
+        
+    def rsample(self) -> Tensor:
+        """sample `z ~ N(z | mu, sigma)` (with the reparameterization trick) """
+        epsilon = self.sample_epsilon()
+        return self.mu + self.sigma * epsilon
+            
+    def log_prob(self, z:Tensor) -> Tensor:
+        """return the log probability: log `p(z)`"""
+        return -0.5 * (math.log(2 * math.pi) + 2 * torch.log(self.sigma) + ((z - self.mu) ** 2) / (self.sigma ** 2))
